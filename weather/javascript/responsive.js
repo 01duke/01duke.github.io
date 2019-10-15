f7ec8bf0c225a8c07bb0cfc9a45735a2454a1e16
01duke <idahodukes@hotmail.com>
2019-10-15 09:01:51 -0700

    th

--- weather/javascript/responsive.js
+++ weather/javascript/responsive.js
@@ -1,3 +1,15 @@
 function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+function getDate() {
+    var date = new Date();
+	const dayNames =
+	["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var dayName = dayNames[date.getDay()];
+	const monthNames =
+	["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var monthName = monthNames[date.getMonth()];
+    document.getElementById("getDate").innerHTML = dayName + ", " + date.getDate() + " " + monthName + " " + date.getFullYear();
+}
+
+window.onload = getDate();
